f662cdf19345a19de655c16ca3da04b8c3c23a51
shimazarei <49499236+shimazarei@users.noreply.github.com>
2020-03-01 11:48:05 +0100

    Update RandomForestmodel.py

--- RandomForestmodel.py
+++ RandomForestmodel.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
